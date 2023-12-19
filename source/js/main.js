9feda732527dadfa480e64010fd34dd0f535507e
Maxim <57897415+max-rad@users.noreply.github.com>
2023-12-19 13:51:27 +0300

    Initial commit

---
+++ source/js/main.js
@@ -0,0 +1,12 @@
+import {initAccordion} from './modules/accordion/init-accordion';
+import {initCustomSelect} from './modules/custom-select/init-custom-select';
+import {initHeader} from './modules/header/init-header';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    initHeader();
+    initCustomSelect();
+    initAccordion();
+  });
+});
