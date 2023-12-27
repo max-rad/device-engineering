2401182ff5c83488c5fd650979f3eb33249895ca
max-rad <radionoffm@ya.ru>
2023-12-27 12:37:49 +0300

    feat: заканчивает шапку, интро

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,13 @@
 import {initAccordion} from './modules/accordion/init-accordion';
 import {initCustomSelect} from './modules/custom-select/init-custom-select';
 import {initHeader} from './modules/header/init-header';
+import {initHeaderAccordion} from './modules/header-accordion/init-header-accordion';
 
 
 window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
     initHeader();
+    initHeaderAccordion();
     initCustomSelect();
     initAccordion();
   });
